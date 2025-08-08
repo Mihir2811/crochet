8189167def10bceeb7ff1a0c90aaf45a54eb604e
Mihir <mihirpanchal2811@gmail.com>
2025-08-08 17:52:05 +0530

    Aug 8

--- script.js
+++ script.js
@@ -56,3 +56,22 @@ document.querySelectorAll('#mobile-nav a').forEach(link => {
     }
   });
 });
+
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  const form = e.target;
+
+  fetch(form.action, {
+    method: 'POST',
+    body: new FormData(form),
+    headers: { 'Accept': 'application/json' }
+  }).then(response => {
+    if (response.ok) {
+      alert('Thanks for your message!');
+      form.reset();
+    } else {
+      alert('Oops! Something went wrong.');
+    }
+  });
+});
+
